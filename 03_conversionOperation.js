9aca7b20946978eb9114e1c33e341067908e41de
Tabish <tabishraj7@gmail.com>
2024-10-10 18:02:34 +0530

    conversion of datatypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score="raj"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// "77" => 77
+// "77abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn="raj"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false
+// "raj" => true
+
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)

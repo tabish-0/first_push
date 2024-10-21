7867b95bddf597e4e62ac5f28a3dce1ad49c10e9
Tabish <tabishraj7@gmail.com>
2024-10-21 14:47:46 +0530

    inheritance

---
+++ 10_classes_and_oop/inheritance.js
@@ -0,0 +1,36 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`)
+    }
+}
+
+class Teacher extends User{
+    constructor(username,email,password){
+        super(username)
+        this.email=email;
+        this.password=password;
+    }
+
+    addCourse(){
+        console.log(`A new courses was added by ${this.username}`)
+    }
+}
+
+const chai = new Teacher("chai","chai@gmail.com","123")
+
+// chai.addCourse()
+chai.logMe()
+
+const masalaChai = new User("masalaChai")
+
+// masalaChai.addCourse()
+masalaChai.logMe()
+
+// console.log(chai === masalaChai)
+// console.log(chai === Teacher)
+console.log(chai instanceof Teacher)
+console.log(chai instanceof User)
